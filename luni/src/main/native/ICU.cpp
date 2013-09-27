MotorolaMobilityLLC/libcore

Explode early when ICU's .dat file isn't available.
Also make it generally more obvious (and more obvious who's to blame) when
libcore's native code fails to initialize.

Bug: 3005342
Change-Id: I75496bb0c15391d7f32ba50b9c488ea0480e3e42

luni/src/main/native/ICU.cpp
@@ -568,5 +568,14 @@ static JNINativeMethod gMethods[] = {
     NATIVE_METHOD(ICU, toUpperCase, "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;"),
 };
 int register_com_ibm_icu4jni_util_ICU(JNIEnv* env) {
+    // Failures to find the ICU data tend to be somewhat obscure because ICU loads its data on first
+    // use, which can be anywhere. Force initialization up front so we can report a nice clear error
+    // and bail.
+    UErrorCode status = U_ZERO_ERROR;
+    u_init(&status);
+    if (status != U_ZERO_ERROR) {
+        LOGE("Couldn't initialize ICU: %s", u_errorName(status));
+        return -1;
+    }
     return jniRegisterNativeMethods(env, "com/ibm/icu4jni/util/ICU", gMethods, NELEM(gMethods));
 }

napodan/android_libcore

Conflicts:
	luni/src/main/native/ICU.cpp
	luni/src/main/native/Register.cpp

luni/src/main/native/ICU.cpp
@@ -568,5 +568,13 @@ static JNINativeMethod gMethods[] = {
     NATIVE_METHOD(ICU, toUpperCase, "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;"),
 };
 void register_libcore_icu_ICU(JNIEnv* env) {
+    // Failures to find the ICU data tend to be somewhat obscure because ICU loads its data on first
+    // use, which can be anywhere. Force initialization up front so we can report a nice clear error
+    // and bail.
+    UErrorCode status = U_ZERO_ERROR;
+    u_init(&status);
+    if (status != U_ZERO_ERROR) {
+        ALOGE("Couldn't initialize ICU: %s", u_errorName(status));
+    }
     jniRegisterNativeMethods(env, "libcore/icu/ICU", gMethods, NELEM(gMethods));
 }